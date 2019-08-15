3bbf4aaeb0ea484e9e891d961850a8ad354a27ee
AlfredYang1986 <yangyuanpig@gmail.com>
2019-08-15 17:03:51 +0800

    面向新的数据结构导入

---
+++ src/test/scala/com/pharbers/NTMIOAggregation/AggTest.scala
@@ -0,0 +1,18 @@
+package com.pharbers.NTMIOAggregation
+
+import org.scalatest.FunSuite
+
+class AggTest extends FunSuite {
+
+    test("test for tm agg") {
+        println("start")
+        println(TmInputAgg("5d5515ae6573f7239ad3cb0f",
+            "5d5515eb12563c156b04f8d5",
+            "5d5515eb12563c156b04f8d6"))
+    }
+
+//    test("test for tm result agg") {
+//        println("start")
+//        println(TmResultAgg("3517b56b-8a39-4b2c-b4f8-0df5aea55e86"))
+//    }
+}
