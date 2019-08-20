adaf7d4c35e1657cd10e107d53e8886c36ee1ecc
Cui <cui_cjm@163.com>
2019-08-20 16:44:31 +0800

    产品状态更新，增加"正在开发"状态

--- src/test/scala/com/pharbers/TmAggregation/AggTest.scala
+++ src/test/scala/com/pharbers/TmAggregation/AggTest.scala
@@ -29,4 +29,13 @@ class AggTest extends FunSuite {
             "5d59110df013b2055287813e",
             "5d59110df013b2055287813f", 0))
     }
+
+    test("test for tm new agg cal_report to report1") {
+        println("start")
+        println(TmAggCal2Report.apply(
+            "7b5cdf82-70e1-4026-8aa8-5296cb48bbca",
+            "5d57ed3cab0bf2192d416afb",
+            "5d59110df013b2055287813e",
+            "5d59110df013b2055287813f", 0))
+    }
 }
