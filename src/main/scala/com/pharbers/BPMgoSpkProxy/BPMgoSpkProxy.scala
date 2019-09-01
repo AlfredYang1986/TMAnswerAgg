8448c717dd1af14741e0e10417cea3b34625a445
AlfredYang1986 <yangyuanpig@gmail.com>
2019-09-01 17:13:07 +0800

    第一步

--- src/main/scala/com/pharbers/BPMgoSpkProxy/BPMgoSpkProxy.scala
+++ src/main/scala/com/pharbers/BPMgoSpkProxy/BPMgoSpkProxy.scala
@@ -4,6 +4,8 @@ import com.mongodb.spark.sql._
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import com.mongodb.spark.config.ReadConfig
+import com.pharbers.TmAggregation.TmAggPreset2Cal
+import org.apache.spark.sql.functions._
 
 class BPMgoSpkProxy {
 
@@ -23,14 +25,36 @@ class BPMgoSpkProxy {
         .set("spark.executor.cores", "1")
         .set("spark.executor.instances", "1")
 
-    def loadDataFromMgo2Spark(): DataFrame = {
+    def loadDataFromMgo2Spark(
+                                 proposalId: String,
+                                 projectId: String,
+                                 periodId: String,
+                                 phase: Int = 0): String = {
+
+        val job_id = TmAggPreset2Cal.apply(proposalId, projectId, periodId, phase)
+
         val spark = SparkSession.builder().config(conf).getOrCreate()
 
-        val readConfig = ReadConfig(Map(
+        val cal_data_rc = ReadConfig(Map(
             "uri" -> ("mongodb://" + mongoHost + ":" + mongoPort + "/"),
             "database" -> destDatabase,
-            "collection" -> "presets"))
+            "collection" -> "cal"))
 
-        spark.read.mongo(readConfig) // 2)
+        val cal_data = spark.read.mongo(cal_data_rc).filter(col("job_id") === job_id).drop(col("_id"))
+        cal_data.show(100)
+        cal_data.write.parquet("hdfs://192.168.100.137:9000/tmtest0831/jobs/" + job_id + "/input/" + "cal_data")
+//        cal_data.write.json("hdfs://192.168.100.137:9000/tmtest0831/jobs/" + job_id + "/json/" + "cal_data")
+
+        val cal_comp_rc = ReadConfig(Map(
+            "uri" -> ("mongodb://" + mongoHost + ":" + mongoPort + "/"),
+            "database" -> destDatabase,
+            "collection" -> "cal_comp"))
+
+        val cal_comp = spark.read.mongo(cal_comp_rc).filter(col("job_id") === job_id).drop(col("_id"))
+        cal_comp.show(100)
+        cal_comp.write.parquet("hdfs://192.168.100.137:9000/tmtest0831/jobs/" + job_id + "/input/" + "cal_comp")
+//        cal_comp.write.json("hdfs://192.168.100.137:9000/tmtest0831/jobs/" + job_id + "/json/" + "cal_comp")
+
+        job_id
     }
 }
--- src/test/scala/com/pharbers/BPMgoSpkProxy/MgoSpkTest.scala
+++ src/test/scala/com/pharbers/BPMgoSpkProxy/MgoSpkTest.scala
@@ -6,13 +6,17 @@ class MgoSpkTest extends FunSuite {
     test("extract data from mongo 2 spark") {
         println("start")
 
-        val mongoHost = System.setProperty("MONGO_HOST", "pharbers.com")
-        val mongoPort = System.setProperty("MONGO_PORT", "5555")
-        val destDatabase = System.setProperty("MONGO_DEST", "pharbers-ntm-client")
+        System.setProperty("MONGO_HOST", "pharbers.com")
+        System.setProperty("MONGO_PORT", "5555")
+        System.setProperty("MONGO_DEST", "pharbers-ntm-client")
 
         val proxy = new BPMgoSpkProxy
-        val result = proxy.loadDataFromMgo2Spark()
-        result.printSchema()
-        result.show()
+        val result = proxy.loadDataFromMgo2Spark(
+            "5d57ed3cab0bf2192d416afb",
+            "5d6b7cb3744610c15e0cf474",
+            "5d6b7cb3744610c15e0cf475",
+            0
+        )
+        println(result)
     }
 }
