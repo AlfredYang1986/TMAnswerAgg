4abd43d51167b85fd0bba2ad8f6454106156040c
AlfredYang1986 <yangyuanpig@gmail.com>
2019-09-06 10:27:27 +0800

    一个函数

--- src/main/java/com/pharbers/CallJMethod/BPTMProxy/TMProxy.java
+++ src/main/java/com/pharbers/CallJMethod/BPTMProxy/TMProxy.java
@@ -1,5 +1,6 @@
 package com.pharbers.CallJMethod.BPTMProxy;
 
+import com.pharbers.BPMgoSpkProxy.BPEsSpkProxyImpl;
 import com.pharbers.BPMgoSpkProxy.BPMgoSpkProxyImpl;
 
 public class TMProxy {
@@ -18,5 +19,14 @@ public class TMProxy {
                                  Number phase) {
 
         BPMgoSpkProxyImpl.loadDataFromSpark2Mgo(jobid, proposalid, projectid, periodid, phase.intValue());
+    }
+
+    public void BPTMUBShowResult(String jobid,
+                                 String proposalid,
+                                 String projectid,
+                                 String periodid,
+                                 Number phase) {
+
+        BPEsSpkProxyImpl.loadDataFromSpark2Es(proposalid, projectid, periodid, phase.intValue());
     }
 }
